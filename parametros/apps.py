18b8e728a1848bdf61dcef2743426319aac3a2b1
Carlos Andrés Herrera <carlos.a.herrera@icloud.com>
2017-10-22 21:04:50 -0500

    Added App Parametros

---
+++ parametros/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ParametrosConfig(AppConfig):
+    name = 'parametros'
--- prefact/urls.py
+++ prefact/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from parametros import views
 
 urlpatterns = [
+    url(r'^$',views.index,name='index'),
     url(r'^admin/', admin.site.urls),
 ]
