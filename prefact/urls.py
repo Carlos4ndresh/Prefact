a67f09ae31ec09be57236bff6a492b9403627adb
Carlos Andrés Herrera <carlos.a.herrera@icloud.com>
2017-10-24 21:24:07 -0500

    Test de views

--- prefact/urls.py
+++ prefact/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from parametros import views
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    # url(r'^$',views.index,name='index'),
+    url(r'^parametros/',include('parametros.urls')),
     url(r'^admin/', admin.site.urls),
 ]
