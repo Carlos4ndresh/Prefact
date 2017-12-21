9c59e45ad2a220f4cf204fa6122af42a33b850c0
Carlos Herrera <carlos.herrera@outlook.com>
2017-12-20 22:06:21 -0500

    pruebas de dependencia

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
-python manage.py migrate                  # Apply database migrations
-python manage.py collectstatic --noinput  # Collect static files
+while ! nc -z mysqldb 33066; 
+    do 
+        echo sleeping; 
+        sleep 1; 
+    done; 
+    python manage.py migrate                  # Apply database migrations
+    python manage.py collectstatic --noinput  # Collect static files
 
-# Prepare log files and start outputting logs to stdout
-touch /srv/logs/gunicorn.log
-touch /srv/logs/access.log
-tail -n 0 -f /srv/logs/*.log &
+    # Prepare log files and start outputting logs to stdout
+    touch /srv/logs/gunicorn.log
+    touch /srv/logs/access.log
+    tail -n 0 -f /srv/logs/*.log &
 
-# Start Gunicorn processes
-echo Starting Gunicorn.
-exec gunicorn prefact.wsgi:application \
-    --name Defina \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --log-level=info \
-    --log-file=/srv/logs/gunicorn.log \
-    --access-logfile=/srv/logs/access.log \
-    "$@"
+    # Start Gunicorn processes
+    echo Starting Gunicorn.
+    exec gunicorn prefact.wsgi:application \
+        --name Defina \
+        --bind 0.0.0.0:8000 \
+        --workers 3 \
+        --log-level=info \
+        --log-file=/srv/logs/gunicorn.log \
+        --access-logfile=/srv/logs/access.log \
+        "$@"
+    echo Connected!;
+
+# python manage.py migrate                  # Apply database migrations
+# python manage.py collectstatic --noinput  # Collect static files
+
+# # Prepare log files and start outputting logs to stdout
+# touch /srv/logs/gunicorn.log
+# touch /srv/logs/access.log
+# tail -n 0 -f /srv/logs/*.log &
+
+# # Start Gunicorn processes
+# echo Starting Gunicorn.
+# exec gunicorn prefact.wsgi:application \
+#     --name Defina \
+#     --bind 0.0.0.0:8000 \
+#     --workers 3 \
+#     --log-level=info \
+#     --log-file=/srv/logs/gunicorn.log \
+#     --access-logfile=/srv/logs/access.log \
+#     "$@"
