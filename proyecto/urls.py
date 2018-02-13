4670443412810bd84af9f7df34db5e60b99234f1
Carlos Herrera <carlos.herrera@outlook.com>
2018-02-12 22:13:25 -0500

    añadidos templates de etapas e inventarios, añadidos botones de cancelar

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -5,11 +5,8 @@ app_name = 'proyecto'
 
 urlpatterns = [
     url(r'^$',views.MacroproyectoListView.as_view(),name='indexProyecto'),
-    # url(r'^proyecto/nuevomacro/$',views.MacroproyectoCreateView.as_view(),name='nuevoMacro'),
     url(r'^nuevomacro/$',views.MacroproyectoCreateView.as_view(),name='nuevoMacro'),
     url(r'^macroproydetalle/(?P<pk>\d+)/edit/$',views.MacroproyectoEditView.as_view(),name='macro_edit'),
-    # url(r'^proyecto/$',views.ProyectoListView.as_view(),name='proyecto_lista'),
-    # url(r'^proyecto/(?P<pk>\d+)/incrementos/$',views.ProyectoIncrementoView.as_view(),name='proyecto_incremento'),
     url(r'^proyecto/(?P<pk>\d+)/lista/$',views.ProyectoListView.as_view(),name='proyecto_list'),
     url(r'^proyecto/(?P<pk>\d+)/incrementos/create/$',views.VentaCreateView.as_view(),name='proyecto_incremento'),
     url(r'^proyecto/(?P<pk>\d+)/incrementos/edit/$',views.VentaUpdateView.as_view(),name='incremento_edit'),
